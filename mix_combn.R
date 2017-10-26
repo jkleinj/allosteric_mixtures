b8dc320b6c75aa0167d6bb5e426db0f5339102b5
jkleinj <jens@jkleinj.eu>
2017-10-26 17:38:20 +0100

    seems to work

--- mix_combn.R
+++ mix_combn.R
@@ -1,33 +1,27 @@
 #! /usr/bin/R
 #================================================================================
 # combinatorics of mixtures
+# see the README file for an explanation of the computations
 # Copyright (C) 2017 Jens Kleinjung
 #================================================================================
 
-## see the README file for an explanation of the computations
-
-#_______________________________________________________________________________
 ## monomer concentrations
 C = 1;
 
 ## enzymatic activities of tetramers
-AC = 2; # allostery coefficient
-aaaa_ea = 4^AC/4;
-aaai_ea = 3^AC/4;
-aaii_ea = 2^AC/4;
-aiii_ea = 1^AC/4;
-iiii_ea = 0^AC/4;
+AC = 4; # allostery coefficient
+aaaa_ea = (4/4)^AC;
+aaai_ea = (3/4)^AC;
+aaii_ea = (2/4);
+aiii_ea = (1/4);
+iiii_ea = (0/4);
 #aaaa_ea = aaai_ea = aaii_ea = aiii_ea = iiii_ea = 1;
 
-rs = seq(0.1, 10, by = 0.1);
+rs = seq(0.1, 10, by = 0.01);
 
 result.ea = lapply(rs, function(r) {
 	a = C / (1 + 1 / r);
 	i = C / (1 + r);
-
-	#print(paste("r", r, "; C", C));
-	#print(paste("a", a, "; i", i));
-
 	sum(a + i);
 
 	#_______________________________________________________________________________
@@ -37,7 +31,6 @@ result.ea = lapply(rs, function(r) {
 	aaii = 6 * a^2 * i^2;
 	aiii = 4 * a * i^3;
 	iiii = i^4;
-
 	stopifnot(isTRUE(all.equal(sum(aaaa + aaai + aaii + aiii + iiii), 1)));
 
 	#_______________________________________________________________________________
@@ -48,16 +41,10 @@ result.ea = lapply(rs, function(r) {
 				aiii * aiii_ea +
 				iiii * iiii_ea;
 
-	#print(paste("aaaa", aaaa, "; aaai", aaai, "; aaii", aaii, "; aiii", aiii, "; iiii", iiii));
 	c(i, total_ea);
 });
 
 result.ea.df = do.call(rbind.data.frame, result.ea);
 colnames(result.ea.df) = c("i", "total_ea");
-
-
-#_______________________________________________________________________________
-## plot results
 plot(abs(result.ea.df[ ,1]), result.ea.df[ ,2]);
-
 
