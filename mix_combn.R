ea741d85b9b38907e78b9b1f4cac29dfb8ace707
jkleinj <jens@jkleinj.eu>
2017-10-26 16:53:46 +0100

    works

--- mix_combn.R
+++ mix_combn.R
@@ -10,11 +10,20 @@
 ## monomer concentrations
 C = 1;
 
-rs = seq(10, 0, by = -0.1);
+## enzymatic activities of tetramers
+AC = 2; # allostery coefficient
+aaaa_ea = 4^AC/4;
+aaai_ea = 3^AC/4;
+aaii_ea = 2^AC/4;
+aiii_ea = 1^AC/4;
+iiii_ea = 0^AC/4;
+#aaaa_ea = aaai_ea = aaii_ea = aiii_ea = iiii_ea = 1;
 
-result.ea = sapply(rs, function(r) {
-	a = N / (1 + 1 / r);
-	i = N / (1 + r);
+rs = seq(0.1, 10, by = 0.1);
+
+result.ea = lapply(rs, function(r) {
+	a = C / (1 + 1 / r);
+	i = C / (1 + r);
 
 	#print(paste("r", r, "; C", C));
 	#print(paste("a", a, "; i", i));
@@ -29,17 +38,10 @@ result.ea = sapply(rs, function(r) {
 	aiii = 4 * a * i^3;
 	iiii = i^4;
 
-	sum(aaaa + aaai + aaii + aiii + iiii);
+	stopifnot(isTRUE(all.equal(sum(aaaa + aaai + aaii + aiii + iiii), 1)));
 
 	#_______________________________________________________________________________
-	## enzymatic activities
-	aaaa_ea = 4/4;
-	aaai_ea = 3/4;
-	aaii_ea = 2/4;
-	aiii_ea = 1/4;
-	iiii_ea = 0/4;
-
-
+	## total enzymatic activity of tetramer mixture
 	total_ea =	aaaa * aaaa_ea +
 				aaai * aaai_ea +
 				aaii * aaii_ea +
@@ -47,13 +49,15 @@ result.ea = sapply(rs, function(r) {
 				iiii * iiii_ea;
 
 	#print(paste("aaaa", aaaa, "; aaai", aaai, "; aaii", aaii, "; aiii", aiii, "; iiii", iiii));
-	c(r, total_ea);
+	c(i, total_ea);
 });
 
-result.ea.mat = as.matrix(t(result_ea));
+result.ea.df = do.call(rbind.data.frame, result.ea);
+colnames(result.ea.df) = c("i", "total_ea");
+
 
 #_______________________________________________________________________________
 ## plot results
-plot(abs(result.ea.mat[ ,1]), result.ea.mat[ ,2]);
+plot(abs(result.ea.df[ ,1]), result.ea.df[ ,2]);
 
 
